68739bb57ef2fe674cbd63cadbf2787af71e29da
makarova1507ana <103330304+makarova1507ana@users.noreply.github.com>
2023-09-13 22:18:24 +0400

    Update lesson1.sql

--- lesson1.sql
+++ lesson1.sql
@@ -10,17 +10,34 @@ INSERT INTO products VALUES(2, "помидоры", "Zarya", 500);
 INSERT INTO products (id, cost) VALUES(3, 1400); 
 # множественная вставка
 INSERT INTO products (id, nameP) VALUES (4, "banana"), (5, "apple"), (6, "lemon"); 
+INSERT INTO products (id, nameP, cost) VALUES (7, "Огурцы", 200), (8, "арбузы", 300), (9, "дыни",700); 
 
 
 SELECT * FROM products; #ПОЛУЧЕНИЕ ДАННЫХ
 SELECT nameP FROM products; #ПОЛУЧЕНИЕ ДАННЫХ
 
+SELECT * FROM products WHERE nameP = "Огурцы"; #ПОЛУЧЕНИЕ ДАННЫХ с уточнением
+SELECT nameP, cost FROM products WHERE cost>300;
 
-/*--------------------ЗАДАНИЕ----------------------------*/
-# Создать таблицу для Customers
-# id (уникальный номер заказчика)
-# name (имя)
-# city (город)
-# rating (рейтинг)
 
-# вставить 3-х разных заказчиков
+# /*--------------------ЗАДАНИЕ----------------------------*/
+# # Создать таблицу для Customers
+# # 	id (уникальный номер заказчика)
+# # 	name (имя)
+# #	city (город)
+# # 	rating (рейтинг)
+# #
+# # вставить 3-х разных заказчиков
+
+# CREATE TABLE Customers(
+# 	id INT NOT NULL, 
+# 	nameC NVARCHAR(50),
+#   	city NVARCHAR(50),
+# 	rating  INT(10));
+    
+# INSERT INTO Customers VALUES (1, "Ivan", "Omsk", 5),  (2, "Ira", "Kirov", 7);
+
+
+# # получение данных
+# SELECT * FROM Customers;
+# SELECT nameC FROM Customers;
